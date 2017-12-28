d3a833efbd6c69d1127848530f216e03aca49f85
Frank Proulx <fproulx@gmail.com>
2017-12-28 06:25:37 -0800

    Proposed refactoring

--- pybna.py
+++ pybna.py
@@ -6,8 +6,10 @@ import network as nx
 import psycopg2
 import pandas as pd
 
+from routing import network
 
-def build_network(conn,tableName=None):
+
+def build_network(conn, tableName=None):
     """
     Returns a networkx graph built from edges and nodes stored in the
     database at the given connection. Assumes the standard naming
@@ -15,3 +17,27 @@ def build_network(conn,tableName=None):
     used to indicate the table to use in place of the standard
     neighborhood_ways table.
     """
+
+class BNA():
+    """A BNA to analyze."""
+    def __init__(self):
+        # Create network
+
+        # Add census blocks
+        self.blocks
+        # Add destinations
+
+    def _buildNetwork(self):
+        """Build a routing.network."""
+        pass
+
+    def _addBlocks(self):
+        """Add census blocks to BNA."""
+        pass
+
+    def _addDestinations(self):
+        """Add generic destinations."""
+        pass
+
+    def score(self):
+        """Calculate network score."""
