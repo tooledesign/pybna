1861cedf648a80f1bbea2c87668cfc567d8718d9
Spencer <spencergardner@gmail.com>
2018-10-26 15:59:28 -0700

    Rework zone creation and add nodes

--- pybna/sql/zones/test.py
+++ pybna/sql/zones/test.py
@@ -1,3 +1,3 @@
 import pybna
 bna = pybna.pyBNA(config="/home/spencer/dev/pybna/pybna/sql/zones/test_config.yaml",verbose=True)
-bna.make_zones('zones','scratch')
+bna.make_zones('zones','scratch',roads_filter="f_class IN ('1','2','3','4','6','7')")
