3b8f17285ec3c18380b9b8b86dc4a4454d0af8e0
S <spencergardner@gmail.com>
2019-07-24 15:45:53 -0700

    Remove bad dependency

--- pybna/stress.py
+++ pybna/stress.py
@@ -7,7 +7,6 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2.extras import DictCursor
 import yaml
-from beautifultable import BeautifulTable
 from tooles import DBUtils
 import pandas as pd
 
