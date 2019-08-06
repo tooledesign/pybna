afebdcf25743115dcb457350e409e5baaa78849d
Spencer Gardner <spencergardner@gmail.com>
2019-08-06 07:34:27 -0700

    SQL Changes; need logic updates

--- pybna/sql/destinations/count_based_score.sql
+++ pybna/sql/destinations/count_based_score.sql
@@ -1,26 +1,42 @@
 --
 -- calculates access to a destination type using counts and score thresholds
 --
-DROP TABLE IF EXISTS pg_temp.d;
+DROP TABLE IF EXISTS pg_temp.tmp_dests;
+CREATE TEMP TABLE pg_temp.tmp_dests AS (
+    SELECT
+        {destination_id} AS id,
+        {geom} AS geom
+    FROM {destinations_schema}.{destinations_table} destinations
+    WHERE {filter}
+);
+CREATE INDEX tsidx_tmp_dests ON pg_temp.tmp_dests USING GIST (geom);
+ANALYZE pg_temp.tmp_dests;
 
-SELECT
-    {destination_id} AS id,
-    unnest({block_id_col}) AS block_id
-INTO TEMP TABLE pg_temp.d
-FROM {schema}.{table};
-CREATE INDEX tidx_d_id ON pg_temp.d (id);
-ANALYZE pg_temp.d;
 
-SELECT
-    connections.{source_block} AS block_id,
-    COUNT(DISTINCT d.id) AS total
-INTO TEMP TABLE pg_temp.{tmp_table}
-FROM
-    {block_connections} connections,
-    pg_temp.d
-WHERE
-    {connection_true}
-    AND connections.{target_block} = d.block_id
-GROUP BY connections.{source_block};
+DROP TABLE IF EXISTS pg_temp.tmp_dest_blocks;
+CREATE TEMP TABLE pg_temp.tmp_dest_blocks AS (
+    SELECT
+        tmp_dests.id AS dest_id,
+        blocks.{blocks_id_col} AS block_id
+    FROM
+        pg_temp.tmp_dests,
+        {blocks_schema}.{blocks_table} blocks
+);
+CREATE INDEX tidx_tmp_dest_blocks ON pg_temp.tmp_dest_blocks (block_id);
+ANALYZE pg_temp.tmp_dest_blocks;
+
+
+CREATE TEMP TABLE pg_temp.{tmp_table} AS (
+    SELECT
+        connections.{source_block} AS block_id,
+        COUNT(DISTINCT tmp_dest_blocks.dest_id) AS total
+    FROM
+        {block_connections} connections,
+        pg_temp.tmp_dest_blocks
+    WHERE
+        {connection_true}
+        AND connections.{target_block} = tmp_dest_blocks.block_id
+    GROUP BY connections.{source_block}
+);
 
 CREATE INDEX {index} ON pg_temp.{tmp_table} (block_id); ANALYZE pg_temp.{tmp_table};
--- pybna/sql/destinations/percentage_based_score.sql
+++ pybna/sql/destinations/percentage_based_score.sql
@@ -1,16 +1,30 @@
 --
 -- calculates access to a destination type from each census block
 --
-SELECT
-    connections.{source_block} AS block_id,
-    SUM(target_block.{val}) AS total
-INTO TEMP TABLE pg_temp.{tmp_table}
-FROM
-    {block_connections} connections,
-    {schema}.{table} target_block
-WHERE
-    {connection_true}
-    AND connections.{target_block} = target_block.{block_id_col}
-GROUP BY connections.{source_block};
+DROP TABLE IF EXISTS pg_temp.tmp_dests;
+CREATE TEMP TABLE pg_temp.tmp_dests AS (
+    SELECT
+        {destination_id} AS id,
+        {val} AS val,
+        {geom} AS geom
+    FROM {destinations_schema}.{destinations_table} destinations
+    WHERE {filter}
+);
+CREATE INDEX tsidx_tmp_dests ON pg_temp.tmp_dests USING GIST (geom);
+ANALYZE pg_temp.tmp_dests;
+
+
+CREATE TEMP TABLE pg_temp.{tmp_table} AS (
+    SELECT
+        connections.{source_block} AS block_id,
+        SUM(target_block.val) AS total
+    FROM
+        {block_connections} connections,
+        pg_temp.tmp_dests target_block
+    WHERE
+        {connection_true}
+        AND connections.{target_block} = target_block.id
+    GROUP BY connections.{source_block}
+);
 
 CREATE INDEX {index} ON pg_temp.{tmp_table} (block_id); ANALYZE pg_temp.{tmp_table};
