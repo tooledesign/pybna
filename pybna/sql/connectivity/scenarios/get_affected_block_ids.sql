6a41e7122dea5967f0981e52b3354d134f513b20
S <spencergardner@gmail.com>
2019-12-11 17:11:44 -0800

    Remove boundary limit

--- pybna/sql/connectivity/scenarios/get_affected_block_ids.sql
+++ pybna/sql/connectivity/scenarios/get_affected_block_ids.sql
@@ -5,9 +5,4 @@ FROM
 WHERE
     roads.{roads_scenario_col} = {scenario_id}
     AND ST_DWithin(blocks.{blocks_geom_col},roads.{roads_geom_col},{connectivity_max_distance})
-    AND EXISTS (
-        SELECT 1
-        FROM {boundary_schema}.{boundary_table} bound
-        WHERE ST_Intersects(blocks.geom,bound.geom)
-    )
 ;
