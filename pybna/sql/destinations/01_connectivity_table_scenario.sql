de137618ee5d75f5ee8b186c9e5e0e833b73cbc8
S <spencergardner@gmail.com>
2019-12-20 16:01:26 -0800

    Fix sorting on connectivity table

--- pybna/sql/destinations/01_connectivity_table_scenario.sql
+++ pybna/sql/destinations/01_connectivity_table_scenario.sql
@@ -10,7 +10,7 @@ CREATE TEMP TABLE pg_temp.tmp_connectivity AS (
     ORDER BY
         source,
         target,
-        (scenario = {scenario_id}) DESC
+        (scenario = {scenario_id}) ASC
 );
 
 CREATE INDEX tidx_conn ON pg_temp.tmp_connectivity (source,target) WHERE low_stress;
