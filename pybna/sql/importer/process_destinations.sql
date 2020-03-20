5c0c6ecfa57f9b7cd6bb9be8f592e46464041787
S <spencergardner@gmail.com>
2020-03-20 14:41:56 -0700

    Handle nested destinations

--- pybna/sql/importer/process_destinations.sql
+++ pybna/sql/importer/process_destinations.sql
@@ -53,3 +53,29 @@ ALTER TABLE {schema}.{final_table} DROP COLUMN geom;
 
 CREATE INDEX {sidx} ON {schema}.{final_table} USING GIST (geom_pt);
 ANALYZE {schema}.{final_table};
+
+-- Remove nested destinations (i.e. destinations that are within a larger
+-- destination such as a playground inside a park)
+DROP TABLE IF EXISTS pg_temp.tmp_d;
+CREATE TEMP TABLE tmp_d AS (
+    SELECT {pkey}
+    FROM {schema}.{final_table} t1
+    WHERE EXISTS (
+        SELECT 1
+        FROM {schema}.{final_table} t2
+        WHERE
+            t1.{pkey} != t2.{pkey}
+            AND ST_Within(t1.geom_pt,t2.geom_poly)
+    )
+);
+
+DELETE FROM {schema}.{final_table}
+WHERE {pkey} IN (SELECT {pkey} FROM pg_temp.tmp_d)
+;
+
+
+
+
+ALTER TABLE {schema}.{final_table} ADD COLUMN {pkey} SERIAL PRIMARY KEY;
+ALTER TABLE {schema}.{final_table} ADD COLUMN geom_pt geometry(point,{srid});
+ALTER TABLE {schema}.{final_table} ADD COLUMN geom_poly geometry(multipolygon,{srid});
