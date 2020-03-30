e86b24ccd9520e6ecab742f8eb76685fbf2ba7c2
Bikingman <daniel.patterson87@gmail.com>
2020-03-30 11:11:54 -0400

    denerated a new yaml file to house lts configuration, fixed a couple typos

--- pybna/testing/pybna_comparison/07_lts_scoring.py
+++ pybna/testing/pybna_comparison/07_lts_scoring.py
@@ -4,15 +4,15 @@
 import pybna
 
 # calculate stress
-bna = pybna.Stress(config='C:\\Users\\dpatterson\\code\\pybna\\pybna\\testing\\pybna_comparison\\config_website.yaml',
+bna = pybna.Stress(config='C:\\Users\\dpatterson\\code\\pybna\\pybna\\testing\\pybna_comparison\\config_website_lts.yaml',
                    create_lookups=True,
                    verbose=False)
 
-s.segment_stress(table="received.updated_segment_stress_scores",
+bna.segment_stress(table="received.updated_segment_stress_scores",
                  table_filter=None,
                  dry=None)
 
-s.crossing_stress(table="received.updated_crossing_stress_scores",
+bna.crossing_stress(table="received.updated_crossing_stress_scores",
                   angle=20,
                   table_filter=None,
                   dry=None)
