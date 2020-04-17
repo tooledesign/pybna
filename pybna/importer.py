6afcb2c716892da9f991619ee9f1eafb38bb59eb
S <spencergardner@gmail.com>
2020-04-17 15:12:34 -0700

    Set LODES year to 2020

--- pybna/importer.py
+++ pybna/importer.py
@@ -313,7 +313,7 @@ class Importer(Conf):
         # copy the shapefile to temporary directory and load into geopandas
         if not state is None:
             print("Loading data for state {}".format(state.upper()))
-            year = 2014
+            year = 2020
             success = False
             while not success:
                 if year < 2010:
